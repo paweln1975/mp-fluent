49b11e999a512aee2a592e456df1b8f8ef64f051
paweln1975 <pawel.niedziela@gmail.com>
2024-02-20 23:58:37 +0100

    vector finished

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 python -m doctest -v data_structs/tests/deck.doctest
+python -m doctest -v data_structs/tests/vector2d.doctest
--- test_run_all.py
+++ test_run_all.py
@@ -4,6 +4,7 @@ import unittest
 
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocFileSuite("data_structs\\tests\\deck.doctest"))
+    tests.addTests(doctest.DocFileSuite("data_structs\\tests\\vector2d.doctest"))
     return tests
 
 
