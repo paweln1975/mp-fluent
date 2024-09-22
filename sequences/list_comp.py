ddeea78e77cb60ca19a6d573e0b2b4c27b4661ca
paweln1975 <pawel.niedziela@gmail.com>
2024-09-22 09:38:30 +0200

    unpacking nested tuples

--- sequences/list_comp.py
+++ sequences/list_comp.py
@@ -118,6 +118,31 @@ class ListExt:
         """
         return first, args
 
+    @staticmethod
+    def unpack_nested_tuple(values_list):
+        """
+        >>> metro_areas = [ \
+                ('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),\
+                ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),\
+                ('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),\
+                ('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),\
+                ('São Paulo', 'BR', 19.649, (-23.547778, -46.635833)),\
+            ]
+        >>> value = ListExt.unpack_nested_tuple(metro_areas)
+        >>> print(value)
+                        |  latitude | longitude
+        Mexico City     |   19.4333 |  -99.1333
+        New York-Newark |   40.8086 |  -74.0204
+        São Paulo       |  -23.5478 |  -46.6358
+        <BLANKLINE>
+        """
+        result = ''
+        result += f'{"":15} | {"latitude":>9} | {"longitude":>9}\n'
+        for name, _, _, (lat, lon) in values_list:
+            if lon <=0:
+                result += f'{name:15} | {lat:9.4f} | {lon:9.4f}\n'
+        return result
+
 class TestListExtComp(unittest.TestCase):
     def setUp(self):
         self.codes_str = '$¢£¥€¤'
